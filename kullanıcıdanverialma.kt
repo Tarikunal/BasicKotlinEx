54537937937d05f4e079f1ccca1373a321224636
Tarık Ünal <tarkunal332@gmail.com>
2018-06-05 23:06:20 +0300

    Create kullanıcıdanverialma.kt

---
+++ kullanıcıdanverialma.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>)
+{
+    print("İsminiz :")
+    var name= readLine()
+    print("Yaşınız :")
+    var yas= readLine()
+    print("Bölümünüz :")
+    var bolum= readLine()
+    print("Not ortalamanız :")
+    var not= readLine()
+    println("Merhaba "+name+"\nYaşınız :"+yas+"\nBölümünüz :"+bolum+"\nAldığınız not :"+not)
+}
